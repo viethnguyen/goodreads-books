02c28120add0ad34e237026a617a3820fc22fa11
Viet Nguyen <viet.nguyen182@gmail.com>
2017-01-07 07:55:01 -0500

    remove temporary response after processing

--- src/Site.hs
+++ src/Site.hs
@@ -8,28 +8,28 @@ module Site
   ( app
   ) where
 
+import Control.Lens
+import Control.Monad.IO.Class
+import Data.ByteString (ByteString)
 ------------------------------------------------------------------------------
 import qualified Data.ByteString.Lazy as BLI
-import Data.ByteString (ByteString)
+import Data.Map.Syntax
 import qualified Data.Text as T
-import Data.Map.Syntax 
 import Heist
 import qualified Heist.Interpreted as I
+import Network.Wreq
 import Snap.Core
 import Snap.Snaplet
 import Snap.Snaplet.Heist
 import Snap.Util.FileServe
-import Network.Wreq
-import Control.Lens
+import System.Directory
 import Text.HTML.TagSoup
 import Text.XmlHtml hiding (render)
-import Control.Monad.IO.Class
 
 ------------------------------------------------------------------------------
 import Application
 
 ------------------------------------------------------------------------------
-
 ------------------------------------------------------------------------------
 -- | The application initializer.
 app :: SnapletInit App App
@@ -41,12 +41,10 @@ app =
 
 -- | The application's routes.
 routes :: [(ByteString, Handler App App ())]
-routes = [ ("/book", bookHandler)
-         , ("media", serveDirectory "static/media")]
-
+routes = [("/book", bookHandler), ("media", serveDirectory "static/media")]
 
 -------------------------------------------------------------------------------
--- | Read Goodreads key and password from a file 
+-- | Read Goodreads key and password from a file
 goodreadsKey :: IO String
 goodreadsKey = fmap (takeWhile (/= '\n')) $ readFile "src/.goodreadskey"
 
@@ -60,28 +58,33 @@ data Book = Book
   , description :: String
   , author :: String
   , comment :: String
-  , link :: String 
+  , link :: String
   } deriving (Eq, Show)
 
 -- | goodread response file name
 goodreadsResFilename :: FilePath
 goodreadsResFilename = "src/.goodreadsresponse"
 
--- | Use wreq to save response body from Goodreads into a file 
+-- | Use wreq to save response body from Goodreads into a file
 saveGoodreadsResponseBody :: IO ()
-saveGoodreadsResponseBody = do
-  -- Get Goodreads key 
+saveGoodreadsResponseBody
+  -- Get Goodreads key
+ = do
   key <- goodreadsKey
-
-  -- Get all the read books 
-  r <-  get ("https://www.goodreads.com/review/list/5285276.xml?key=" ++ key ++ "&v=2&shelf=read&sort=date_read&per_page=200")
+  -- Get all the read books
+  r <-
+    get
+      ("https://www.goodreads.com/review/list/5285276.xml?key=" ++
+       key ++ "&v=2&shelf=read&sort=date_read&per_page=200")
   BLI.writeFile goodreadsResFilename (r ^. responseBody)
 
 -- | For testing, a sample book review. A book review is a collection of TagSoup tags between the two tags <review> and </review>
 sampleBookReview :: IO [Tag String]
 sampleBookReview = do
   tags <- parseTags <$> readFile goodreadsResFilename
-  let br = takeWhile (~/= ("</review>"::String)) $ dropWhile (~/= ("<review>"::String)) tags
+  let br =
+        takeWhile (~/= ("</review>" :: String)) $
+        dropWhile (~/= ("<review>" :: String)) tags
   return br
 
 -- | All reviews from the file in which we save the Goodreads response.
@@ -89,91 +92,117 @@ getBookReviews :: IO [[Tag String]]
 getBookReviews = do
   tags <- parseTags <$> readFile goodreadsResFilename
   let brs = go [] tags
-  return $ reverse brs 
-  where go bs [] = bs
-        go bs tags = let br = takeWhile (~/= ("</review>"::String)) $ drop 1 $ dropWhile (~/= ("<review>"::String)) tags
-                         remain = drop 1 $ dropWhile (~/= ("</review>"::String)) tags
-                      in if (length br == 0) then go bs remain else go (br : bs) remain
+  return $ reverse brs
+  where
+    go bs [] = bs
+    go bs tags =
+      let br =
+            takeWhile (~/= ("</review>" :: String)) $
+            drop 1 $ dropWhile (~/= ("<review>" :: String)) tags
+          remain = drop 1 $ dropWhile (~/= ("</review>" :: String)) tags
+      in if (length br == 0)
+           then go bs remain
+           else go (br : bs) remain
 
 -- | A single book to test
 sampleBook :: IO Book
 sampleBook = do
   br <- sampleBookReview
   return $ parseReview br
-  
 
 -- | parse a review to get book data
 parseReview :: [Tag String] -> Book
 parseReview rev =
-  let t = case takeWhile (~/= ("</title>" :: String)) $ drop 1 $ dropWhile (~/= ("<title>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
-      iu = case takeWhile (~/= ("</image_url>" :: String)) $ drop 1 $ dropWhile (~/= ("<image_url>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
-      d = case takeWhile (~/= ("</description>" :: String)) $ drop 1 $ dropWhile (~/= ("<description>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
-      a = case takeWhile (~/= ("</name>" :: String)) $ drop 1 $ dropWhile (~/= ("<name>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
-      c = case takeWhile (~/= ("</body>" :: String)) $ drop 1 $ dropWhile (~/= ("<body>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
-      l = case takeWhile (~/= ("</link>" :: String)) $ drop 1 $ dropWhile (~/= ("<link>"::String)) rev of
-        [ ] -> ""
-        t : _ -> case maybeTagText t of
-          Nothing -> ""
-          Just s -> (s :: String)
+  let t =
+        case takeWhile (~/= ("</title>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<title>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
+      iu =
+        case takeWhile (~/= ("</image_url>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<image_url>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
+      d =
+        case takeWhile (~/= ("</description>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<description>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
+      a =
+        case takeWhile (~/= ("</name>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<name>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
+      c =
+        case takeWhile (~/= ("</body>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<body>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
+      l =
+        case takeWhile (~/= ("</link>" :: String)) $
+             drop 1 $ dropWhile (~/= ("<link>" :: String)) rev of
+          [] -> ""
+          t:_ ->
+            case maybeTagText t of
+              Nothing -> ""
+              Just s -> (s :: String)
   in Book t iu d a c l
 
--- | test data, a list of Books 
+-- | test data, a list of Books
 books :: [Book]
-books = [
-  Book {
-      title = "Learn You a Haskell",
-      image_url = " ",
-      description = "Beginner book in Haskell",
-      author = "Miran Lipova",
-      comment = "a good beginner book",
-      link = ""
-      },
-  Book {
-      title = "Parallel and Concurrent Programming in Haskell",
-      image_url = " ",
-      description = "Good book in parallel and concurrent Haskell",
-      author = "Simon Marlow",
-      comment = "nice, short book",
-      link = ""
-      }
-  ] 
-      
+books =
+  [ Book
+    { title = "Learn You a Haskell"
+    , image_url = " "
+    , description = "Beginner book in Haskell"
+    , author = "Miran Lipova"
+    , comment = "a good beginner book"
+    , link = ""
+    }
+  , Book
+    { title = "Parallel and Concurrent Programming in Haskell"
+    , image_url = " "
+    , description = "Good book in parallel and concurrent Haskell"
+    , author = "Simon Marlow"
+    , comment = "nice, short book"
+    , link = ""
+    }
+  ]
+
 -- | handler
 bookHandler :: Handler App App ()
 bookHandler = do
   liftIO saveGoodreadsResponseBody
-  brs <- liftIO getBookReviews 
+  brs <- liftIO getBookReviews
+  liftIO $ removeFile goodreadsResFilename
   let bs = map parseReview brs
   renderWithSplices "book" (allBooksSplices bs)
 
--- | convert a list of books to splices 
+-- | convert a list of books to splices
 allBooksSplices :: [Book] -> Splices (SnapletISplice App)
 allBooksSplices bs = "allBooks" ## (renderBooks bs)
 
 renderBooks :: [Book] -> SnapletISplice App
 renderBooks = I.mapSplices $ I.runChildrenWith . splicesFromBook
 
-splicesFromBook ::Monad n => Book -> Splices (I.Splice n)
+splicesFromBook
+  :: Monad n
+  => Book -> Splices (I.Splice n)
 splicesFromBook b = do
   "bookTitle" ## I.textSplice (T.pack $ stripTags $ title b)
   "bookImageUrl" ## I.textSplice (T.pack $ stripTags $ image_url b)
@@ -182,11 +211,10 @@ splicesFromBook b = do
   "bookComment" ## I.textSplice (T.pack $ stripTags $ comment b)
   "bookLink" ## I.textSplice (T.pack $ stripTags $ link b)
 
--- | strip all HTML tags inside a string 
+-- | strip all HTML tags inside a string
 stripTags :: String -> String
-stripTags s = foldr (++) [] $ map fromTagText $ filter pred $ parseTags s 
-  where pred (TagOpen _ _) = False
-        pred (TagClose _) = False
-        pred _ = True
-        
-
+stripTags s = foldr (++) [] $ map fromTagText $ filter pred $ parseTags s
+  where
+    pred (TagOpen _ _) = False
+    pred (TagClose _) = False
+    pred _ = True
